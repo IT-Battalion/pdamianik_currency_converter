3021723dd68672597e710a512620ba19b3979475
pdamianik <39028343+pdamianik@users.noreply.github.com>
2024-01-22 13:10:22 +0100

    Make Source an abstract class with the abc package

--- currencyconverter/model/source.py
+++ currencyconverter/model/source.py
@@ -1,4 +1,7 @@
-class Source:
+from abc import ABC, abstractmethod
+
+
+class Source(ABC):
 
     """
     This class is the base class for all sources. It contains the methods that are required for a source to work.
@@ -13,41 +16,47 @@ class Source:
         """
         self.config = config
 
+    @abstractmethod
     def close(self):
         """
         This method is called when the source is closed. It is used to close any open connections/files.
         """
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def available_currencies(self) -> tuple[int, dict[str, str]]:
         """
         This returns the available currencies and the index of the default currency.
         :return: A tuple containing the index of the default currency and a dictionary that contains all the available
         currencies.
         """
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def add_target_currency(self, currency: str):
         """
         This method is called when a target currency is added.
         :param currency: The currency that is added.
         """
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def remove_target_currency(self, currency: str):
         """
         This method is called when a target currency is removed.
         :param currency: The currency that is removed.
         """
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def source_currency(self, currency: str):
         """
         This method is called when the source currency is changed.
         :param currency: The new source currency.
         """
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def convert(self, amount: float) -> tuple[str, list[tuple[str, float, float]]]:
         """
         This method is called when the user wants to convert an amount of money.
@@ -55,11 +64,11 @@ class Source:
         :return: A tuple containing the date of the conversion and a list of tuples containing the target currency, the
         converted amount and the exchange rate.
         """
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def config_changed(self):
         """
         This method is called when the configuration has changed.
         """
-        raise NotImplementedError
-
+        pass
